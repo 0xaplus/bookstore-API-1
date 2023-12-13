0b28da6418fd56d3a0995787bd16fe584e245d49
0xaplus <codewithaplus@gmail.com>
2023-12-13 21:48:54 +0100

    Updated Schema

--- models/book.js
+++ models/book.js
@@ -2,13 +2,28 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const BookModel = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    year: {
-        type: Number,
-        required: true
-    }
+const BookModelSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+    max: [2022, "Year must be less than or equal to 2022"],
+  },
+  isbn: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  lastUpdatedAt: {
+    type: Date,
+    default: Date.now,
+  }
 });
+
+module.exports = mongoose.Model("books", BookModelSchema);
